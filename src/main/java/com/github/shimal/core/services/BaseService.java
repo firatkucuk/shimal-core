bf6e46b5396d0ba1b1ec9efee27822b633eef6a2
firatkucuk <firatkucuk@gmail.com>
2012-11-01 21:35:19 +0200

    Pagination modified.

--- src/main/java/com/github/shimal/core/services/BaseService.java
+++ src/main/java/com/github/shimal/core/services/BaseService.java
@@ -2,9 +2,12 @@
 package com.github.shimal.core.services;
 
 import com.github.shimal.commons.dao.GenericDAO;
+import com.github.shimal.core.utils.QueryPaginator;
 import java.util.List;
 import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -31,5 +34,20 @@ public class BaseService {
         Integer pageLength = (Integer) params.get("iDisplayLength");
 
         return dao.select(cl, query, pageStart, pageLength);
+    }
+
+
+
+    //~ ----------------------------------------------------------------------------------------------------------------
+
+    @Transactional
+    public <T> List<T> paginate(RowMapper<T> mapper, String query, Map<String, Object> params,
+        QueryPaginator queryPaginator) {
+
+        Integer      pageStart  = (Integer) params.get("iDisplayStart");
+        Integer      pageLength = (Integer) params.get("iDisplayLength");
+        JdbcTemplate jdbcDao    = dao.newJdbcTemplate();
+
+        return jdbcDao.query(queryPaginator.transformSql(query, pageStart, pageLength), mapper);
     }
 }
